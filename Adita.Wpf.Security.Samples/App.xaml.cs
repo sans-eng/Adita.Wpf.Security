052c904f30d65e3f0e0e98dbe01c333c2dd55bb8
Setya Adi Kurnia <engineering.ins@weq-tech.com>
2022-10-07 14:13:15 +0700

    Added sample project

---
+++ Adita.Wpf.Security.Samples/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Adita.Security.Claims;
+using Adita.Security.Principal;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Windows;
+using ApplicationIdentity = Adita.Security.Claims.ApplicationIdentity;
+
+namespace Adita.Wpf.Security.Samples
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            ApplicationIdentity applicationIdentity = new("password");
+            applicationIdentity.AddClaim(new(ClaimTypes.Role, "Admin"));
+
+            ApplicationPrincipal applicationPrincipal = new(applicationIdentity);
+            AppDomain.CurrentDomain.SetThreadPrincipal(applicationPrincipal);
+        }
+    }
+}
